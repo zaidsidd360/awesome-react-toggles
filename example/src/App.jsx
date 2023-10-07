07843f44079c1b74ade18d53a376d953db6fc748
zaidsidd360 <zaidsidd360@gmail.com>
2023-10-08 04:36:33 +0530

    cntd. developement :construction:

--- example/src/App.jsx
+++ example/src/App.jsx
@@ -1,13 +1,10 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import ToggleSwitch from "../../src";
 
 function App() {
   return (
     <div className="container">
-      <ToggleSwitch />
+      <ToggleSwitch size={10} />
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,72 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
-const ToggleSwitch = () => {
-  return (
-    <Label htmlFor='checkbox'>
-      <input type="checkbox" name="checkbox" id="checkbox" />
-      <span></span>
-    </Label>
-  )
+interface ToggelSwitchPropType {
+  size?: number;
+  onToggle?: () => void;
+  isToggled?: boolean;
+  buttonShadow?: string;
+  backgroundShadow?: string;
+  outlineOnFocus?: boolean;
+  outlineColor?: string;
+  outlineWidth?: string;
+  bgColorChecked: string;
+  bgColorUnchecked?: string;
 }
 
-const Label = styled.label`
-  width: 10rem;
-  height: 5rem;
+const ToggleSwitch = ({
+  size = 3.2,
+  onToggle,
+  isToggled,
+  buttonShadow = "rgba(0, 0, 0, 0.35) 0px 5px 15px",
+  backgroundShadow = "rgba(50, 50, 93, 0.25) 0px 15px 30px -15px inset, rgba(0, 0, 0, 0.3) 0px 18px 36px -18px inset",
+  outlineOnFocus = true,
+  outlineColor = "gray",
+  outlineWidth = "1px",
+  bgColorChecked = "#4cda64",
+  bgColorUnchecked = "#dbdbdb",
+}: ToggelSwitchPropType) => {
+  const multiplier = size < 4 ? 0.25 : 0;
+
+  return (
+    <Label
+      htmlFor="checkbox"
+      size={size}
+      multiplier={multiplier}
+      buttonShadow={buttonShadow}
+      backgroundShadow={backgroundShadow}
+      outlineOnFocus={outlineOnFocus}
+      outlineColor={outlineColor}
+      outlineWidth={outlineWidth}
+      bgColorChecked={bgColorChecked}
+      bgColorUnchecked={bgColorUnchecked}
+    >
+      <input
+        onChange={onToggle}
+        checked={isToggled}
+        type="checkbox"
+        name="checkbox"
+        id="checkbox"
+      />
+      <span></span>
+    </Label>
+  );
+};
+
+interface LabelPropType {
+  size: number;
+  multiplier: number;
+  buttonShadow: string;
+  backgroundShadow: string;
+  outlineOnFocus: boolean;
+  outlineColor: string;
+  outlineWidth: string;
+  bgColorChecked: string;
+  bgColorUnchecked: string;
+}
+
+const Label = styled.label<LabelPropType>`
+  height: ${(props) => props.size + "rem"};
+  width: ${(props) => props.size * 2 - 1 + props.multiplier + "rem"};
   display: inline-block;
   position: relative;
 
@@ -20,10 +75,17 @@ const Label = styled.label`
     width: 0;
     height: 0;
     &:checked + span::before {
-      transform: translateX(calc(95/100 * 5rem + 0.5rem/2));
+      /* transform: translateX(calc(95/100 * 5rem + 0.5rem/2)); */
+      transform: translateX(100%);
     }
     &:checked + span {
-      background-color: #84fe88;
+      background-color: ${(props) => props.bgColorChecked};
+    }
+    &:focus + span {
+      border: ${(props) =>
+        props.outlineOnFocus
+          ? `${props.outlineWidth} solid ${props.outlineColor}`
+          : "none"};
     }
   }
 
@@ -31,24 +93,24 @@ const Label = styled.label`
     position: absolute;
     cursor: pointer;
     inset: 0;
-    background-color: #dbdbdb;
+    background-color: ${(props) => props.bgColorUnchecked};
     display: flex;
     align-items: center;
     border-radius: 10rem;
     transition: background-color 200ms ease;
-    box-shadow: rgba(50, 50, 93, 0.25) 0px 15px 30px -15px inset, rgba(0, 0, 0, 0.3) 0px 18px 36px -18px inset;
-
+    box-shadow: ${(props) => props.backgroundShadow};
     &::before {
       position: absolute;
-      left: 0.5rem;
-      content: '';
-      width: 4rem;
+      content: "";
+      left: ${(props) => 0.5 - props.multiplier / 2 + "rem"};
+      width: ${(props) => props.size - 1 + props.multiplier + "rem"};
       aspect-ratio: 1;
       background-color: white;
       border-radius: 50%;
       transition: all 200ms ease;
+      box-shadow: ${(props) => props.buttonShadow};
     }
   }
-`
+`;
 
 export default ToggleSwitch;
